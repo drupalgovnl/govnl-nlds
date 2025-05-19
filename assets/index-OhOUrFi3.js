77a7767b38a5cd0377a7201b53fad7e7e587568b
r1kkert <r1kkert@users.noreply.github.com>
2025-05-19 13:45:36 +0000

    Deploying to gh-pages from @ drupalgovnl/govnl-nlds@da5daf14b73e510a8251943198f7b2d2e4e6b36a 🚀

---
+++ assets/index-OhOUrFi3.js
@@ -0,0 +1,1 @@
+import"./preview-BJUTOJdk.js";import"./DocsRenderer-CFRXHY34-E9R_WFkf.js";const{definePreview:i}=__STORYBOOK_MODULE_PREVIEW_API__;
---
+++ assets/notice-C03NPeki.js
@@ -0,0 +1,19 @@
+import{j as e,M as o,a as i}from"./DocsRenderer-CFRXHY34-E9R_WFkf.js";import{useMDXComponents as a}from"./index-DbBT2VLk.js";import"./index-OhOUrFi3.js";import"./iframe-Bqa4OHvM.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";import"./preview-BJUTOJdk.js";const s=`# Auteursrecht {auteur}
+
+Copyright (c) {jaar} {auteur}
+
+## Logo en huisstijl
+
+Op het huisstijl en logo zijn auteursrechten van toepassing. Het gebruik van logo en huisstijl is alleen toegestaan voor gebruik door {auteur}.
+
+Wanneer je een bewerking van de software wilt gebruiken voor andere doeleinden, mag je niet het logo van {auteur} gebruiken en je ontwerpt een eigen huisstijl.
+
+## Lettertype
+
+Lettertypes die worden gebruikt voor de huisstijl zijn niet allemaal gratis en open source. Let op dat bij gebruik van die bijgeleverde lettertypes je een (betaalde) licentie regelt. Pas anders de configuratie aan om minder of andere lettertypes te gebruiken.
+
+## Toestemming
+
+Wanneer je het logo of de huisstijl wilt gebruiken kun je voor toestemming contact opnemen met {contactpersoon}.
+`;function r(n){return e.jsxs(e.Fragment,{children:[e.jsx(o,{title:"Informatie/Toestemming voor gebruik"}),`
+`,e.jsx(i,{children:s})]})}function h(n={}){const{wrapper:t}={...a(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(r,{...n})}):r()}export{h as default};
