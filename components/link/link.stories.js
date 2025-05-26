3bd567863308160389c0d5af26ba577473574964
Martijn Fokken <m.fokken@dictu.nl>
2025-05-26 12:25:29 +0200

    feat: Adds Link component

---
+++ components/link/link.stories.js
@@ -0,0 +1,44 @@
+import readme from './README.md?raw';
+import '@dictu/design-tokens/dist/link.css';
+import './dist/index.css';
+import { BADGES } from '@dictu/storybook/config/preview';
+
+export default {
+  args: {
+    href: 'dictu.nl'
+  },
+  argTypes: {
+    innerHTML: {
+      control: 'text',
+    },
+    href: {
+      control: 'text',
+    },
+  },
+  parameters: {
+    badges: [BADGES.WIP],
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+  render: ({ innerHTML, href }) => {
+    const classes = ['dictu-link'];
+    const $link = document.createElement('a');
+
+    $link.innerText = innerHTML;
+    $link.href = href;
+    $link.classList.add(...classes);
+
+    return $link;
+  },
+  tags: ['autodocs'],
+  title: 'Componenten/Link',
+};
+
+export const Link = {
+  args: {
+    innerHTML: 'Dit is een link'
+  },
+};
