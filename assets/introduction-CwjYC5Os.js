1218e904675bc625fa7e7c999b25ce6b8c120b0a
MLRFokken <147042740+MLRFokken@users.noreply.github.com>
2025-08-21 14:47:57 +0000

    Deploying to gh-pages from @ drupalgovnl/govnl-nlds@d7c6cf7e99b1752d1586e8dfffbb2bef167e153a 🚀

--- assets/introduction-CwjYC5Os.js
+++ assets/introduction-CwjYC5Os.js
@@ -1,4 +1,4 @@
-import{j as e,b as s,c as a}from"./blocks-Bc1QVbGE.js";import{useMDXComponents as r}from"./index-DuJio7bD.js";import"./iframe-C2SGFds6.js";import"./_commonjsHelpers-CqkleIqs.js";const o=`<!-- @license CC0-1.0 -->
+import{j as e,b as s,c as a}from"./blocks-BZlXQzqD.js";import{useMDXComponents as r}from"./index-L7JM_uo0.js";import"./iframe-CgR-pGjB.js";import"./_commonjsHelpers-CqkleIqs.js";const o=`<!-- @license CC0-1.0 -->
 
 # NL Design System introductie
 
