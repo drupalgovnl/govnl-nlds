42c32a0b9be8a61816aefaa6f186339c6215e4bf
jerschipper <matroosjeroen@gmail.com>
2026-01-16 15:11:35 +0100

    feat(checkbox): update checkbox component with custom appearance and … (#239)
    * feat(checkbox): update checkbox component with custom appearance and checkmark icon
    
    * feat(checkbox): refactor checkbox component to use CSS for checkmark icon and introduce local CSS variables
    
    * feat(checkbox): update checkmark icon SVG
    
    ---------
    
    Co-authored-by: Jeroen Schipper <j.schipper@dictu.nl>

---
+++ .changeset/petite-mice-bake.md
@@ -0,0 +1,6 @@
+---
+"@dictu/checkbox": minor
+---
+
+Removed icon component as child from checkbox. Replaced with CSS ::after logic.
+Introduced new local css variables for checkbox icons.
--- components/checkbox/checkbox.component.js
+++ components/checkbox/checkbox.component.js
@@ -1,11 +1,11 @@
 export function Checkbox({ id, checked }) {
   const classes = ['dictu-checkbox', 'dictu-focus-ring'];
-  const $checkbox = document.createElement('input');
+  const checkbox = document.createElement('input');
 
-  $checkbox.id = id;
-  $checkbox.setAttribute('type', 'checkbox');
-  $checkbox.checked = checked;
-  $checkbox.classList.add(...classes);
+  checkbox.id = id;
+  checkbox.setAttribute('type', 'checkbox');
+  checkbox.checked = checked;
+  checkbox.classList.add(...classes);
 
-  return $checkbox;
+  return checkbox;
 }
