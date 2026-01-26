247885223e01732e2004891cf37dac19f0855233
MLRFokken <147042740+MLRFokken@users.noreply.github.com>
2026-01-26 10:04:33 +0100

    Feat(navigation-bar): Adds submenu functionality (#243)
    * feat(navigation-bar): Adds base for submenu
    
    * fix: adds design tokens for navigation bar submenu
    
    * feat(navigation-bar): Temp commit for navigation bar dropdown menu
    
    * feat(navigation-bar): Adds submenu to navigation bar component
    
    * Adds changeset
    
    * feat(navigation-bar): Refactors mobile styling for navigation bar
    
    * feat(navigation-bar): Adds documentation
    
    * feat(navigation-bar): Update mobile submenu component heights for better visibility
    
    * feat(navigation-bar): Changed navigation bar to improve submenu variant
    
    * feat(navigation-bar): Adds minor fixes to navigation submenu
    
    ---------
    
    Co-authored-by: Kyra de Vries <59610499+kyradv@users.noreply.github.com>
    Co-authored-by: Jeroen Schipper <j.schipper@dictu.nl>

---
+++ components/navigation-bar/navigation-bar.component.js
@@ -0,0 +1,135 @@
+import { Icon } from '../icon/icon.component';
+
+export const NavigationBar = ({
+  items = [],
+  expanded = false,
+  isMobile = false,
+  menuId,
+  classNames = [],
+}) => {
+  const navigationBar = document.createElement('nav');
+  navigationBar.classList.add('dictu-navigation-bar', ...classNames);
+  navigationBar.setAttribute('aria-label', 'Hoofdnavigatie');
+  navigationBar.setAttribute('role', 'navigation');
+
+  // Adds mobile menu toggle.
+  const toggleIconOpen =
+    '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M3 6C3 5.44772 3.44772 5 4 5H20C20.5523 5 21 5.44772 21 6C21 6.55228 20.5523 7 20 7H4C3.44772 7 3 6.55228 3 6ZM3 12C3 11.4477 3.44772 11 4 11H20C20.5523 11 21 11.4477 21 12C21 12.5523 20.5523 13 20 13H4C3.44772 13 3 12.5523 3 12ZM3 18C3 17.4477 3.44772 17 4 17H20C20.5523 17 21 17.4477 21 18C21 18.5523 20.5523 19 20 19H4C3.44772 19 3 18.5523 3 18Z" /></svg>';
+  const toggleIconClose =
+    '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 14 14" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M0.292893 0.292893C0.683417 -0.0976311 1.31658 -0.0976311 1.70711 0.292893L7 5.58579L12.2929 0.292893C12.6834 -0.0976311 13.3166 -0.0976311 13.7071 0.292893C14.0976 0.683417 14.0976 1.31658 13.7071 1.70711L8.41421 7L13.7071 12.2929C14.0976 12.6834 14.0976 13.3166 13.7071 13.7071C13.3166 14.0976 12.6834 14.0976 12.2929 13.7071L7 8.41421L1.70711 13.7071C1.31658 14.0976 0.683417 14.0976 0.292893 13.7071C-0.0976311 13.3166 -0.0976311 12.6834 0.292893 12.2929L5.58579 7L0.292893 1.70711C-0.0976311 1.31658 -0.0976311 0.683417 0.292893 0.292893Z" /></svg>';
+  const menuToggle = document.createElement('button');
+  menuToggle.classList.add('dictu-navigation-bar__toggle');
+  menuToggle.setAttribute('aria-controls', menuId);
+  menuToggle.setAttribute('aria-expanded', expanded);
+  menuToggle.setAttribute('aria-haspopup', true);
+  menuToggle.innerText = 'Menu';
+  menuToggle.insertAdjacentElement(
+    'afterbegin',
+    Icon({
+      icon: expanded ? toggleIconClose : toggleIconOpen,
+      classes: ['dictu-navigation-bar__menu-icon'],
+    })
+  );
+
+  navigationBar.appendChild(menuToggle);
+
+  // Adds navigation items.
+  navigationBar.appendChild(createNavigationList(items, isMobile, menuId, expanded));
+
+  return navigationBar;
+};
+
+const createNavigationList = (items, isMobile, menuId, expanded) => {
+  const navigationList = document.createElement('ul');
+  navigationList.classList.add('dictu-navigation-bar__list');
+  navigationList.id = menuId;
+  navigationList.setAttribute('role', 'menubar');
+
+  if (isMobile && !expanded) {
+    navigationList.classList.add('dictu-navigation-bar__list--hidden');
+  }
+
+  items.forEach(item => {
+    navigationList.appendChild(createNavigationItem(item));
+  });
+
+  return navigationList;
+};
+
+const createNavigationItem = item => {
+  const navigationItem = document.createElement('li');
+  navigationItem.classList.add('dictu-navigation-bar__item');
+
+  if ('children' in item && item.children != null) {
+    navigationItem.appendChild(createNavigationSubmenuToggler(item.title, item.id, item.expanded));
+    navigationItem.appendChild(createNavigationSubmenu(item.children, item.id, item.expanded));
+  } else {
+    navigationItem.appendChild(createNavigationLink(item));
+  }
+
+  return navigationItem;
+};
+
+const createNavigationLink = item => {
+  const navigationLink = document.createElement('a');
+  navigationLink.classList.add('dictu-navigation-bar__link', 'dictu-focus-ring');
+  navigationLink.setAttribute('role', 'menuitem');
+  navigationLink.href = item.link;
+  navigationLink.innerHTML = item.title;
+
+  return navigationLink;
+};
+
+const createNavigationSubmenu = (items, id, expanded = false) => {
+  const navigationSubmenu = document.createElement('div');
+  navigationSubmenu.classList.add('dictu-navigation-bar__submenu');
+  navigationSubmenu.id = id;
+  navigationSubmenu.appendChild(createNavigationSubmenuList(items));
+
+  if (!expanded) {
+    navigationSubmenu.classList.add('dictu-navigation-bar__submenu--hidden');
+  }
+
+  return navigationSubmenu;
+};
+
+const createNavigationSubmenuToggler = (label, id, expanded = false) => {
+  const navigationToggle = document.createElement('button');
+  navigationToggle.classList.add('dictu-navigation-bar__submenu-toggler', 'dictu-focus-ring');
+  navigationToggle.innerHTML = label;
+  navigationToggle.setAttribute('aria-controls', id);
+  navigationToggle.setAttribute('aria-expanded', expanded);
+  navigationToggle.setAttribute('aria-haspopup', true);
+  const icon =
+    '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"/></svg>';
+  const toggleIcon = Icon({ icon });
+  navigationToggle.appendChild(toggleIcon);
+
+  return navigationToggle;
+};
+
+const createNavigationSubmenuList = items => {
+  const navigationSubmenuList = document.createElement('ul');
+  navigationSubmenuList.classList.add('dictu-navigation-bar__submenu-list');
+  navigationSubmenuList.setAttribute('role', 'menu');
+
+  items.forEach(item => {
+    const navigationSubmenuItem = document.createElement('li');
+    navigationSubmenuItem.classList.add('dictu-navigation-bar__submenu-item');
+
+    const navigationSubmenuLink = document.createElement('a');
+    navigationSubmenuLink.classList.add(
+      'dictu-navigation-bar__link',
+      'dictu-navigation-bar__submenu-link',
+      'dictu-focus-ring'
+    );
+    navigationSubmenuLink.href = item.link;
+    navigationSubmenuLink.innerText = item.label;
+    navigationSubmenuLink.setAttribute('role', 'menuitem');
+
+    navigationSubmenuItem.appendChild(navigationSubmenuLink);
+    navigationSubmenuList.appendChild(navigationSubmenuItem);
+  });
+
+  return navigationSubmenuList;
+};
--- components/navigation-bar/navigation-bar.stories.js
+++ components/navigation-bar/navigation-bar.stories.js
@@ -1,61 +1,8 @@
 import readme from './README.md?raw';
 import './dist/index.css';
+import { NavigationBar } from './navigation-bar.component';
 
 export default {
-  argTypes: {
-    items: {
-      control: 'array',
-    },
-  },
-  parameters: {
-    docs: {
-      description: {
-        component: readme,
-      },
-    },
-  },
-  tags: ['autodocs'],
-  title: 'Componenten/NavigationBar',
-};
-
-export const NavigationBar = {
-  render: args => {
-    // Responsive, accessible, JS-free navigation bar
-    const navigationBar = document.createElement('nav');
-    const { items } = args || [];
-    navigationBar.classList.add('dictu-navigation-bar');
-    navigationBar.setAttribute('aria-label', 'Hoofdnavigatie');
-    navigationBar.setAttribute('role', 'navigation');
-    navigationBar.innerHTML = `
-      <input type="checkbox" id="nav-toggle" class="dictu-navigation-bar__toggle" aria-controls="nav-list" aria-label="Menu tonen/verbergen" />
-      <a href="#" class="dictu-navigation-bar__link dictu-navigation-bar__home" aria-label="Home">
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" clip-rule="evenodd" d="M11.2929 2.29289C11.6834 1.90237 12.3166 1.90237 12.7071 2.29289L21.7071 11.2929C21.9931 11.5789 22.0787 12.009 21.9239 12.3827C21.7691 12.7564 21.4045 13 21 13H20V19C20 20.6569 18.6569 22 17 22H7.00003C5.34318 22 4.00003 20.6569 4.00003 19V13H3.00003C2.59557 13 2.23093 12.7564 2.07615 12.3827C1.92137 12.009 2.00692 11.5789 2.29292 11.2929L11.2929 2.29289ZM11 14C10.4477 14 10 14.4477 10 15V20H14V15C14 14.4477 13.5523 14 13 14H11ZM18 19C18 19.5523 17.5523 20 17 20H16V15C16 13.3431 14.6569 12 13 12H11C9.34318 12 8.00003 13.3431 8.00003 15V20H7.00003C6.44775 20 6.00003 19.5523 6.00003 19L6.00003 12C6.00003 11.5712 5.73013 11.2054 5.35094 11.0633L12 4.41421L18.6491 11.0633C18.2699 11.2054 18 11.5712 18 12L18 19Z" fill="#154273"/>
-        </svg>
-      </a>
-      <label for="nav-toggle" class="dictu-navigation-bar__menu-label" tabindex="0">
-        <span class="dictu-navigation-bar__menu-icon" aria-hidden="true">
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" clip-rule="evenodd" d="M3 6C3 5.44772 3.44772 5 4 5H20C20.5523 5 21 5.44772 21 6C21 6.55228 20.5523 7 20 7H4C3.44772 7 3 6.55228 3 6ZM3 12C3 11.4477 3.44772 11 4 11H20C20.5523 11 21 11.4477 21 12C21 12.5523 20.5523 13 20 13H4C3.44772 13 3 12.5523 3 12ZM3 18C3 17.4477 3.44772 17 4 17H20C20.5523 17 21 17.4477 21 18C21 18.5523 20.5523 19 20 19H4C3.44772 19 3 18.5523 3 18Z" fill="#154273"/>
-        </svg>
-        </span>
-      </label>
-      <ul class="dictu-navigation-bar__list" id="nav-list" role="menubar">
-        ${items
-          .map(
-            item => `
-          <li class="dictu-navigation-bar__item" role="none">
-            <a href="${item.link}" class="dictu-navigation-bar__link dictu-focus-ring" role="menuitem" tabindex="0">
-              <span class="dictu-navigation-bar__title">${item.title}</span>
-            </a>
-          </li>
-        `
-          )
-          .join('')}
-      </ul>
-    `;
-    return navigationBar;
-  },
   args: {
     items: [
       {
@@ -71,5 +18,130 @@ export const NavigationBar = {
         title: 'Rijksoverheid',
       },
     ],
+    expanded: false,
+    isMobile: false,
+    menuId: 'default-menu',
+  },
+  argTypes: {
+    items: {
+      control: 'array',
+    },
+    expanded: {
+      control: 'boolean',
+    },
+    isMobile: {
+      control: 'boolean',
+    },
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+  component: NavigationBar,
+  tags: ['wip'],
+  title: 'Componenten/Navigation Bar',
+};
+
+export const NavigationBarDefault = {};
+
+export const NavigationBarDefaultMobile = {
+  globals: {
+    viewport: { value: 'mobile1', isRotated: false },
+  },
+  args: {
+    expanded: false,
+    isMobile: true,
+    menuId: 'default-mobile-menu',
+  },
+};
+
+export const NavigationBarDefaultMobileExpanded = {
+  globals: {
+    viewport: { value: 'mobile1', isRotated: false },
+  },
+  args: {
+    expanded: true,
+    isMobile: true,
+    menuId: 'default-mobile-menu',
+  },
+};
+
+export const NavigationBarSubmenu = {
+  args: {
+    items: [
+      {
+        link: 'https://www.dictu.nl',
+        title: 'Dienst ICT Uitvoering',
+      },
+      {
+        title: 'Submenu',
+        id: 'submenu-1',
+        children: [
+          {
+            link: '#',
+            title: 'Submenu item 1',
+          },
+          {
+            link: '#',
+            title: 'Submenu item 2',
+          },
+          {
+            link: '#',
+            title: 'Submenu item 3',
+          },
+          {
+            link: '#',
+            title: 'Submenu item 4',
+          },
+        ],
+      },
+      {
+        link: 'https://www.rijksoverheid.nl',
+        title: 'Rijksoverheid',
+      },
+    ],
+    expanded: true,
+  },
+};
+
+export const NavigationBarSubmenuOpen = {
+  args: {
+    items: [
+      {
+        link: 'https://www.dictu.nl',
+        title: 'Dienst ICT Uitvoering',
+      },
+      {
+        title: 'Submenu',
+        id: 'submenu-1',
+        expanded: true,
+        children: [
+          {
+            href: '#',
+            label: 'Subpagina 1',
+          },
+          {
+            href: '#',
+            label: 'Subpagina 2',
+          },
+          {
+            href: '#',
+            label: 'Subpagina 3',
+          },
+          {
+            href: '#',
+            label: 'Subpagina 4',
+          },
+        ],
+      },
+      {
+        link: 'https://www.rijksoverheid.nl',
+        title: 'Rijksoverheid',
+      },
+    ],
+    expanded: true,
   },
 };
