43408536b615a9759d7217659366f6e4c915eed6
MLRFokken <147042740+MLRFokken@users.noreply.github.com>
2026-03-25 10:18:45 +0000

    Deploying to gh-pages from @ drupalgovnl/govnl-nlds@034e9ce5ba8adc9b76a7e63fa9073c9c220b4486 🚀

---
+++ assets/link-list.stories-CHiIo87n.js
@@ -0,0 +1,50 @@
+/* empty css              *//* empty css              *//* empty css              */import{L as s}from"./link-list.component-C6_S2g_r.js";import"./link.component-CBfmraUq.js";import"./icon.component-X7CMXdF1.js";const o=`<!-- @license CC0-1.0 -->
+
+Link list uitleg
+
+## Gebruik deze component
+
+Je kunt de CSS zo in je project installeren:
+
+\`\`\`console
+npm install --save-dev @dictu/link-list
+\`\`\`
+
+Je kunt de CSS uit \`node_modules/\` importeren:
+
+\`\`\`html
+<link rel="stylesheet" href="node_modules/@dictu/link-list/dist/index.css" />
+\`\`\`
+
+Als je CSS imports gebruikt vanuit JavaScript:
+
+\`\`\`javascript
+import "@dictu/link-list/dist/index.css";
+\`\`\`
+
+## Richtlijnen
+
+Tekst toevoegen
+
+## Links
+
+### Relevante WCAG regels
+
+Aanpassen:
+
+- [WCAG eis 1.3.1](https://www.w3.org/TR/WCAG21/#info-and-relationships)
+- [WCAG eis 1.4.12](https://www.w3.org/TR/WCAG21/#text-spacing)
+`,m={argTypes:{items:{control:"object"}},parameters:{docs:{description:{component:o}}},component:s,tags:["autodocs"],title:"Componenten/Link List"},n={args:{items:[{label:"Contact",href:"dictu.nl"},{label:"Colofon",href:"dictu.nl"},{label:"Toegankelijkheid",href:"dictu.nl"}]}};var e,t,i;n.parameters={...n.parameters,docs:{...(e=n.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    items: [{
+      label: 'Contact',
+      href: 'dictu.nl'
+    }, {
+      label: 'Colofon',
+      href: 'dictu.nl'
+    }, {
+      label: 'Toegankelijkheid',
+      href: 'dictu.nl'
+    }]
+  }
+}`,...(i=(t=n.parameters)==null?void 0:t.docs)==null?void 0:i.source}}};const u=["DefaultLinkList"];export{n as DefaultLinkList,u as __namedExportsOrder,m as default};
---
+++ assets/paragraph.component-MjdFU0yj.js
@@ -0,0 +1,1 @@
+const e=({text:r,classNames:t=[]})=>{const a=document.createElement("p");return a.classList.add("dictu-paragraph",...t),a.innerText=r,a};export{e as P};
