61239235a0d67b082818b8c6b1c756bd984b7521
jerschipper <matroosjeroen@gmail.com>
2026-04-03 14:36:03 +0000

    Deploying to gh-pages from @ drupalgovnl/govnl-nlds@8d5fcc03528a4dfb468ba586d413f7384407df7f 🚀

---
+++ assets/formatter-EIJCOSYU-BcJNuCjs.js
@@ -0,0 +1,1 @@
+import{d as m,c as e}from"./blocks-ZTMERZbY.js";import{b as o}from"./iframe-C8ivg0Uh.js";import"./preload-helper-Dp1pzeXC.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
