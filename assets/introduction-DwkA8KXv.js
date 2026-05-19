54bf9fdadc5d7e16d56d77daf3c9cc4d00a763a5
jerschipper <matroosjeroen@gmail.com>
2026-05-19 12:04:53 +0000

    Deploying to gh-pages from @ drupalgovnl/govnl-nlds@3d96da77d61dc82822af3408c0215a7888b18e2c 🚀

--- assets/introduction-DwkA8KXv.js
+++
@@ -1,2 +0,0 @@
-import{j as t,M as r,T as s}from"./blocks-D8vmWVMK.js";import{useMDXComponents as i}from"./index-C41g2vEM.js";import"./preload-helper-Dp1pzeXC.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-BzM7v5uh.js";function o(e){return t.jsxs(t.Fragment,{children:[t.jsx(r,{title:"Voor ontwikkelaars/Introductie"}),`
-`,t.jsx(s,{children:"DICTU Design System voor ontwikkelaars"})]})}function x(e={}){const{wrapper:n}={...i(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(o,{...e})}):o()}export{x as default};
