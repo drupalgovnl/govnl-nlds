fc69731e4322e4b6bb882e57ba14732c0910ca5a
jerschipper <matroosjeroen@gmail.com>
2026-05-19 10:17:46 +0000

    Deploying to gh-pages from @ drupalgovnl/govnl-nlds@371d0acd72b1679d760e63fd270f7e5ce7f64c48 🚀

---
+++ assets/team-Dbdy31Aw.js
@@ -0,0 +1,8 @@
+import{j as e,M as o}from"./blocks-C-FSsFbp.js";import{useMDXComponents as i}from"./index-O0Wkk4cg.js";import"./preload-helper-Dp1pzeXC.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-nRAaduhq.js";function t(r){const n={h1:"h1",h2:"h2",p:"p",...i(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(o,{title:"Informatie/Team"}),`
+`,e.jsx(n.h1,{id:"dictu-design-systeem-team",children:"DICTU Design Systeem Team"}),`
+`,e.jsx(n.h2,{id:"kyra-de-vries",children:"Kyra de Vries"}),`
+`,e.jsx(n.p,{children:"UX designer"}),`
+`,e.jsx(n.h2,{id:"jeroen-schipper",children:"Jeroen Schipper"}),`
+`,e.jsx(n.p,{children:"Frontend developer"}),`
+`,e.jsx(n.h2,{id:"martijn-fokken",children:"Martijn Fokken"}),`
+`,e.jsx(n.p,{children:"Frontend developer"})]})}function p(r={}){const{wrapper:n}={...i(),...r.components};return n?e.jsx(n,{...r,children:e.jsx(t,{...r})}):t(r)}export{p as default};
