39e9584be70fa11cacb2f6879a05bcc4ee115112
MLRFokken <147042740+MLRFokken@users.noreply.github.com>
2026-05-29 08:30:55 +0000

    Deploying to gh-pages from @ drupalgovnl/govnl-nlds@549c9a0e26865ce729a6de5e4084663392557d00 🚀

---
+++ assets/checkbox-jUGDg28X.js
@@ -0,0 +1,53 @@
+import{j as e,M as d,T as h,S as a,C as t,A as l,a as m}from"./blocks-ntC6CR6t.js";import{useMDXComponents as c}from"./index-Du-kUDrR.js";import{C as i,D as r,a as x}from"./checkbox.stories-BnVmIPId.js";import"./preload-helper-Dp1pzeXC.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-ffUAXBd8.js";import"./checkbox.component-PLTYKzR5.js";/* empty css              */const j=`<!-- @license CC0-1.0 -->
+
+## De Checkbox component implementeren
+
+Je kunt de CSS zo in je project installeren:
+
+\`\`\`console
+npm install --save-dev @dictu/checkbox
+\`\`\`
+
+Je kunt de CSS uit \`node_modules/\` importeren:
+
+\`\`\`html
+<link rel="stylesheet" href="node_modules/@dictu/checkbox/dist/index.css" />
+\`\`\`
+
+Als je CSS imports gebruikt vanuit JavaScript:
+
+\`\`\`javascript
+import "@dictu/checkbox/dist/index.css";
+\`\`\`
+`;function s(o){const n={a:"a",h2:"h2",h3:"h3",li:"li",p:"p",ul:"ul",...c(),...o.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(d,{of:i,title:"Componenten/Form elements/Checkbox"}),`
+`,e.jsx(h,{children:"Checkbox"}),`
+`,e.jsx(a,{children:e.jsx(n.p,{children:"De Input component wordt gebruikt één of meerdere opties van een set aan opties te kiezen."})}),`
+`,e.jsx(t,{of:r,sourceState:"none"}),`
+`,e.jsx(n.h2,{id:"parameters",children:"Parameters"}),`
+`,e.jsx(l,{of:i}),`
+`,e.jsx(m,{children:j}),`
+`,e.jsx(n.h2,{id:"wanneer-gebruik-je-de-checkbox-component",children:"Wanneer gebruik je de Checkbox component"}),`
+`,e.jsx(n.p,{children:`Gebruik de Checkbox component voor een keuzemogelijkheid waarbij meerdere opties gekozen kunnen worden, of bij een enkele opt-in mogelijkheid, zoals bijvoorbeeld het akkoord
+gaan met algemene voorwaarden.`}),`
+`,e.jsx(n.h2,{id:"wanneer-gebruik-je-de-checkbox-component-niet",children:"Wanneer gebruik je de Checkbox component niet"}),`
+`,e.jsxs(n.p,{children:["Gebruik de Checkbox component niet wanneer er uit een lijst met keuzes slechts één optie moet worden gekozen. Hiervoor is het ",e.jsx(n.a,{href:"?path=/docs/componenten-form-elements-radio-button--docs",children:"Radio Button component"})," bedoelt."]}),`
+`,e.jsx(n.h2,{id:"richtlijnen",children:"Richtlijnen"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Gebruik de Checkbox component altijd in combinatie met de ",e.jsx(n.a,{href:"?path=/docs/componenten-form-elements-label--docs",children:"Label component"})]}),`
+`]}),`
+`,e.jsx(n.h2,{id:"voorbeelden",children:"Voorbeelden"}),`
+`,e.jsx(n.h3,{id:"checkbox",children:"Checkbox"}),`
+`,e.jsx(t,{of:r}),`
+`,e.jsx(n.h3,{id:"aangevinkte-checkbox",children:"Aangevinkte checkbox"}),`
+`,e.jsx(t,{of:x}),`
+`,e.jsx(n.h2,{id:"relevante-wcag-succescriteria",children:"Relevante WCAG succescriteria"}),`
+`,e.jsx(n.p,{children:"(Later toevoegen)"}),`
+`,e.jsx(n.h2,{id:"help-mee-om-deze-component-te-verbeteren",children:"Help mee om deze component te verbeteren"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Neem deel aan de discussie over de ",e.jsx(n.a,{href:"",children:"Checkbox component op GitHub"}),"."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"",children:"Biedt een wijzigingsvoorstel aan op Github"}),". Lees hoe je het beste ",e.jsx(n.a,{href:"",children:"een wijziging kunt aanbieden"}),"."]}),`
+`]}),`
+`,e.jsx(n.h2,{id:"heb-je-hulp-nodig",children:"Heb je hulp nodig?"}),`
+`,e.jsxs(n.p,{children:["Heb je een vraag over deze component of het DICTU Design System? ",e.jsx(n.a,{href:"?path=/docs/informatie-team--docs",children:"Neem dan contact op met het team"}),"."]})]})}function w(o={}){const{wrapper:n}={...c(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(s,{...o})}):s(o)}export{w as default};
--- assets/introduction-BoLYOvd0.js
+++
@@ -1,2 +0,0 @@
-import{j as t,M as r,T as s}from"./blocks-vi9iTwoo.js";import{useMDXComponents as i}from"./index-5CnEG06a.js";import"./preload-helper-Dp1pzeXC.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-BrWF2wyO.js";function o(e){return t.jsxs(t.Fragment,{children:[t.jsx(r,{title:"Voor ontwikkelaars/Introductie"}),`
-`,t.jsx(s,{children:"DICTU Design System voor ontwikkelaars"})]})}function x(e={}){const{wrapper:n}={...i(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(o,{...e})}):o()}export{x as default};
