123f2b937fee5f726601006bb77b90a3ccfc9488
Keltoken <k.brans@dictu.nl>
2026-07-21 13:33:22 +0000

    Deploying to gh-pages from @ drupalgovnl/govnl-nlds@853dea0fe049f473f89a8e0286d1f1cd925d5032 🚀

---
+++ assets/introduction-2MqY9sHv.js
@@ -0,0 +1,2 @@
+import{j as t,M as r,T as s}from"./blocks-CY_p43zK.js";import{useMDXComponents as i}from"./index-CgA6CdwZ.js";import"./preload-helper-Dp1pzeXC.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-C5R7Qg0M.js";function o(e){return t.jsxs(t.Fragment,{children:[t.jsx(r,{title:"Voor ontwikkelaars/Introductie"}),`
+`,t.jsx(s,{children:"DICTU Design System voor ontwikkelaars"})]})}function x(e={}){const{wrapper:n}={...i(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(o,{...e})}):o()}export{x as default};
