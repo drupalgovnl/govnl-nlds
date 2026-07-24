3909189c40e32cf4fd91430ada822beed5653d93
k-brans <k.brans@dictu.nl>
2026-07-24 08:59:09 +0000

    Deploying to gh-pages from @ drupalgovnl/govnl-nlds@021edc546188258d5ce8f02fc9e2e8f234eb235d 🚀

---
+++ assets/radio-button-CrynL8xp.js
@@ -0,0 +1,55 @@
+import{j as e,M as a,T as c,S as l,C as o,A as h,a as m}from"./blocks-DjWRRmkL.js";import{useMDXComponents as d}from"./index-BDxXqMvM.js";import{R as i,D as r,C as u}from"./radio-button.stories-BRlfiXz8.js";import"./preload-helper-Dp1pzeXC.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-HDCVVCMF.js";/* empty css              */const j=`<!-- @license CC0-1.0 -->
+
+## De Radio Button component implementeren
+
+Je kunt de CSS zo in je project installeren:
+
+\`\`\`console
+npm install --save-dev @dictu/radio-button
+\`\`\`
+
+Je kunt de CSS uit \`node_modules/\` importeren:
+
+\`\`\`html
+<link rel="stylesheet" href="node_modules/@dictu/radio-button/dist/index.css" />
+\`\`\`
+
+Als je CSS imports gebruikt vanuit JavaScript:
+
+\`\`\`javascript
+import "@dictu/radio-button/dist/index.css";
+\`\`\`
+`;function s(t){const n={a:"a",h2:"h2",h3:"h3",li:"li",p:"p",ul:"ul",...d(),...t.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(a,{of:i,title:"Componenten/Form elements/Radio Button"}),`
+`,e.jsx(c,{children:"Radio Button"}),`
+`,e.jsx(l,{children:e.jsx(n.p,{children:"De Input component wordt gebruikt één of meerdere opties van een set aan opties te kiezen."})}),`
+`,e.jsx(o,{of:r,sourceState:"none"}),`
+`,e.jsx(n.h2,{id:"parameters",children:"Parameters"}),`
+`,e.jsx(h,{of:i}),`
+`,e.jsx(m,{children:j}),`
+`,e.jsx(n.h2,{id:"wanneer-gebruik-je-de-checkbox-component",children:"Wanneer gebruik je de Checkbox component"}),`
+`,e.jsx(n.p,{children:"Gebruik de Radio Button component voor een keuzemogelijkheid waarbij slechts één optie gekozen kan worden."}),`
+`,e.jsx(n.h2,{id:"wanneer-gebruik-je-de-checkbox-component-niet",children:"Wanneer gebruik je de Checkbox component niet"}),`
+`,e.jsxs(n.p,{children:[`Gebruik de Radio Button component niet wanneer de gebruiker uit een lijst met keuzes meerdere opties mag selecteren, of wanneer er slechts één keuze is, zoals bij het akkoord
+gaan met algemene voorwaarden. Hiervoor is het `,e.jsx(n.a,{href:"?path=/docs/componenten-form-elements-checkbox--docs",children:"Checkbox component"})," bedoelt."]}),`
+`,e.jsx(n.h2,{id:"richtlijnen",children:"Richtlijnen"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Gebruik de Radio Button component altijd in combinatie met de ",e.jsx(n.a,{href:"?path=/docs/componenten-form-elements-label--docs",children:"Label component"})]}),`
+`,e.jsxs(n.li,{children:[`Radio buttons worden altijd gebruikt in een groep van radio buttons waarin slechts één radio button de checked status mag hebben. Gebruik radio buttons daarom altijd in een
+`,e.jsx(n.a,{href:"?path=/docs/componenten-form-elements-fieldset--docs",children:"Fieldset component"}),"."]}),`
+`]}),`
+`,e.jsx(n.h2,{id:"voorbeelden",children:"Voorbeelden"}),`
+`,e.jsx(n.h3,{id:"radio-button",children:"Radio Button"}),`
+`,e.jsx(o,{of:r}),`
+`,e.jsx(n.h3,{id:"geselecteerde-radio-button",children:"Geselecteerde Radio Button"}),`
+`,e.jsx(o,{of:u}),`
+`,e.jsx(n.h2,{id:"relevante-wcag-succescriteria",children:"Relevante WCAG succescriteria"}),`
+`,e.jsx(n.p,{children:"(Later toevoegen)"}),`
+`,e.jsx(n.h2,{id:"help-mee-om-deze-component-te-verbeteren",children:"Help mee om deze component te verbeteren"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Neem deel aan de discussie over de ",e.jsx(n.a,{href:"",children:"Radio Button component op GitHub"}),"."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"",children:"Biedt een wijzigingsvoorstel aan op Github"}),". Lees hoe je het beste ",e.jsx(n.a,{href:"",children:"een wijziging kunt aanbieden"}),"."]}),`
+`]}),`
+`,e.jsx(n.h2,{id:"heb-je-hulp-nodig",children:"Heb je hulp nodig?"}),`
+`,e.jsxs(n.p,{children:["Heb je een vraag over deze component of het DICTU Design System? ",e.jsx(n.a,{href:"?path=/docs/informatie-team--docs",children:"Neem dan contact op met het team"}),"."]})]})}function C(t={}){const{wrapper:n}={...d(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(s,{...t})}):s(t)}export{C as default};
---
+++ assets/textarea-v6MTtPqF.js
@@ -0,0 +1,67 @@
+import{j as e,M as d,T as l,S as c,C as t,A as h,a as x}from"./blocks-DjWRRmkL.js";import{useMDXComponents as s}from"./index-BDxXqMvM.js";import{T as a,a as i,b as j,c as m,d as p,e as u,f as b}from"./textarea.stories-CJG5sjJj.js";import"./preload-helper-Dp1pzeXC.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-HDCVVCMF.js";/* empty css              */const g=`<!-- @license CC0-1.0 -->
+
+## De Textarea component implementeren
+
+Je kunt de CSS zo in je project installeren:
+
+\`\`\`console
+npm install --save-dev @dictu/textarea
+\`\`\`
+
+Je kunt de CSS uit \`node_modules/\` importeren:
+
+\`\`\`html
+<link rel="stylesheet" href="node_modules/@dictu/textarea/dist/index.css" />
+\`\`\`
+
+Als je CSS imports gebruikt vanuit JavaScript:
+
+\`\`\`javascript
+import "@dictu/textarea/dist/index.css";
+\`\`\`
+`;function o(r){const n={a:"a",code:"code",h2:"h2",h3:"h3",li:"li",p:"p",strong:"strong",ul:"ul",...s(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(d,{of:a,title:"Componenten/Form elements/Textarea"}),`
+`,e.jsx(l,{children:"Textarea"}),`
+`,e.jsx(c,{}),`
+`,e.jsx(t,{of:i,sourceState:"none"}),`
+`,e.jsx(n.h2,{id:"parameters",children:"Parameters"}),`
+`,e.jsx(h,{of:a}),`
+`,e.jsx(x,{children:g}),`
+`,e.jsx(n.h2,{id:"wanneer-gebruik-je-de-textareacomponent",children:"Wanneer gebruik je de Textareacomponent"}),`
+`,e.jsx(n.p,{children:"Gebruik de Textarea component wanneer de gebruiker tekst moet invoeren dat langer is dan één regel, zoals bijvoorbeeld een beschrijving of uitleg."}),`
+`,e.jsx(n.h2,{id:"wanneer-gebruik-je-de-text-input-component-niet",children:"Wanneer gebruik je de Text Input component niet"}),`
+`,e.jsx(n.p,{children:"Gebruik de Textarea component niet wanneer het verwachte antwoord van de gebruiker bestaat uit tekst op slechts één regel. Gebruik dan de Text Input component."}),`
+`,e.jsx(n.h2,{id:"wanneer-gebruik-je-de-textarea-component-niet",children:"Wanneer gebruik je de Textarea component niet"}),`
+`,e.jsx(n.h2,{id:"richtlijnen",children:"Richtlijnen"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Gebruik de Textarea component in combinatie met de ",e.jsx(n.a,{href:"?path=/docs/componenten-form-elements-label--docs",children:"Label component"})]}),`
+`]}),`
+`,e.jsx(n.h2,{id:"voorbeelden",children:"Voorbeelden"}),`
+`,e.jsx(n.h3,{id:"textarea",children:"Textarea"}),`
+`,e.jsx(t,{of:i}),`
+`,e.jsx(n.h3,{id:"rows",children:"Rows"}),`
+`,e.jsxs(n.p,{children:["Met het ",e.jsx(n.code,{children:"rows"})," attribuut kun je de hoogte van de textarea bepalen. Standaard staat deze waarde op ",e.jsx(n.code,{children:"2"}),"."]}),`
+`,e.jsx(t,{of:j}),`
+`,e.jsx(n.h3,{id:"cols",children:"Cols"}),`
+`,e.jsxs(n.p,{children:["Met het ",e.jsx(n.code,{children:"cols"})," attribuut kun je de breedte van de textarea bepalen. Standaard staat deze waarde op ",e.jsx(n.code,{children:"20"}),"."]}),`
+`,e.jsx(t,{of:m}),`
+`,e.jsx(n.h3,{id:"textarea-met-foutieve-invoer",children:"Textarea met foutieve invoer"}),`
+`,e.jsxs(n.p,{children:["Wanneer je visueel wilt laten zien dat de invoer van de textarea foutief is, voeg dan de class ",e.jsx(n.code,{children:"dictu-textarea--error"})," toe aan het textarea element."]}),`
+`,e.jsx(t,{of:p}),`
+`,e.jsx(n.h3,{id:"disabled",children:"Disabled"}),`
+`,e.jsx(t,{of:u}),`
+`,e.jsx(n.h3,{id:"textarea-met-placeholder",children:"Textarea met placeholder"}),`
+`,e.jsxs(n.p,{children:["Het is mogelijk om een placeholder te laten zien in de textarea. Voeg hiervoor het ",e.jsx(n.code,{children:"placeholder"})," attribuut toe aan het ",e.jsx(n.code,{children:"textarea"})," element."]}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.strong,{children:"Let op:"}),` Een placeholder is nooit een vervanging voor het Label elemement. Gebruik alleen een placeholder als de informatie niet op een andere manier
+weergegeven kan worden. Een placeholder verdwijnt namelijk wanneer de gebruiker het inputveld vult. Hierdoor is het niet mogelijk om de informatie uit
+de placeholder tussentijds nogmaals te bekijken. Ook wanneer er een foutmelding wordt weergegeven, is het voor gebruikers niet mogelijk om te controleren
+wat er precies fout is.`]}),`
+`,e.jsx(t,{of:b}),`
+`,e.jsx(n.h2,{id:"relevante-wcag-succescriteria",children:"Relevante WCAG succescriteria"}),`
+`,e.jsx(n.p,{children:"(nog in te vullen)"}),`
+`,e.jsx(n.h2,{id:"help-mee-om-deze-component-te-verbeteren",children:"Help mee om deze component te verbeteren"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Neem deel aan de discussie over de ",e.jsx(n.a,{href:"",children:"Textarea component op GitHub"}),"."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"",children:"Biedt een wijzigingsvoorstel aan op Github"}),". Lees hoe je het beste ",e.jsx(n.a,{href:"",children:"een wijziging kunt aanbieden"}),"."]}),`
+`]}),`
+`,e.jsx(n.h2,{id:"heb-je-hulp-nodig",children:"Heb je hulp nodig?"}),`
+`,e.jsxs(n.p,{children:["Heb je een vraag over deze component of het DICTU Design System? ",e.jsx(n.a,{href:"?path=/docs/informatie-team--docs",children:"Neem dan contact op met het team"}),"."]})]})}function z(r={}){const{wrapper:n}={...s(),...r.components};return n?e.jsx(n,{...r,children:e.jsx(o,{...r})}):o(r)}export{z as default};
