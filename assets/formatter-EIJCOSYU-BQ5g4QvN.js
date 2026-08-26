2f036f0fae6d68df19a6e987d921fad75ca1cebf
k-brans <k.brans@dictu.nl>
2026-08-26 07:29:22 +0000

    Deploying to gh-pages from @ drupalgovnl/govnl-nlds@e3019fa002c6e28bc09a076239d490e3fed648e9 🚀

---
+++ assets/formatter-EIJCOSYU-BQ5g4QvN.js
@@ -0,0 +1,1 @@
+import{d as m,c as e}from"./blocks-D8G0M9NT.js";import{b as o}from"./iframe-DZKgDg1_.js";import"./preload-helper-Dp1pzeXC.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
