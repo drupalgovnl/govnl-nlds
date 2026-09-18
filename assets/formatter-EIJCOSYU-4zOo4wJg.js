4d58a0efedb27e02e2398655c0d8bdbb3f01c3d3
erikruiter <96722557+erikruiter@users.noreply.github.com>
2026-09-18 11:07:16 +0000

    Deploying to gh-pages from @ drupalgovnl/govnl-nlds@a0ff303089ea52ec9a61965c58461c2f64014121 🚀

---
+++ assets/formatter-EIJCOSYU-4zOo4wJg.js
@@ -0,0 +1,1 @@
+import{d as m,c as e}from"./blocks-CpXXIh26.js";import{b as o}from"./iframe-BF61XZrB.js";import"./preload-helper-Dp1pzeXC.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
